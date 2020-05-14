7c0a51952c2be68a4e5ad0349a3954a8c7c26b57
Vladimir Joshkin <vladimirjoshkin@gmail.com>
2020-05-15 00:01:54 +0800

    menu cards now displayed horizontally

--- static/menu.js
+++ static/menu.js
@@ -1,10 +1,10 @@
 function resizeMenu() {
     $('.menu-row').each(function() {
-        var cards_count = $(this).length;
-        var card_width = $(window).width * 0.85 / cards_count;
-        $(this).width(cards_width);
+        var cards_count = $(this).children('.menu-card').length;
+        var card_width = $(window).width() * 0.85 / cards_count;
+        $(this).children('.menu-card').width(card_width);
     });
 };
 
 $(document).ready(resizeMenu);
-$(window).on('resize', resizeMenu)
+$(window).on('resize', resizeMenu);
