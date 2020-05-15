d31ab872c23d60130c8038d490215ae40f2dbd11
Vladimir Joshkin <vladimirjoshkin@gmail.com>
2020-05-15 19:24:48 +0800

    second and third row added

--- static/menu.js
+++ static/menu.js
@@ -10,20 +10,17 @@ $(document).ready(resizeMenu);
 $(window).on('resize', resizeMenu);
 
 $(document).ready(function() {
-    $($('.menu-row')[0]).css('margin-top', '25px');
-    $($('.menu-row')[$('.menu-row').length - 1]).css('margin-bottom', '25px');
-    /*
     $('.menu-row').each(function() {
-        for (var i = 1; i < $(this).children('.menu-card').length - 1; i++) {
-            $($(this).children('.menu-card')[i]).css('margin-right', '25px').css('margin-left', '25px');
-        }
-    });
-    */
-    $('.menu-row').each(function() {
+        $(this).css('margin-top', '25px').css('margin-bottom', '25px');
         $(this).children('.menu-card').css('margin-right', '12px').css('margin-left', '12px');
     });
+
+    $($('.menu-row')[0]).css('margin-top', '25px').css('margin-bottom', '25px');
+    $($('.menu-row')[$('.menu-row').length - 1]).css('margin-top', '25px').css('margin-bottom', '25px');
 });
 
+/*
 for (var i = 1; i < $('.menu-row').length - 1; i++) {
     $($('.menu-row')[i]).css('margin-top', '25px').css('margin-bottom', '25px');
 }
+*/
