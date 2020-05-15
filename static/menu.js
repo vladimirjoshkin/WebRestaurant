3977200a4f8394e24a2743045fc2002a7c24afcf
Vladimir Joshkin <vladimirjoshkin@gmail.com>
2020-05-15 19:15:50 +0800

    menu card logic added

--- static/menu.js
+++ static/menu.js
@@ -8,3 +8,22 @@ function resizeMenu() {
 
 $(document).ready(resizeMenu);
 $(window).on('resize', resizeMenu);
+
+$(document).ready(function() {
+    $($('.menu-row')[0]).css('margin-top', '25px');
+    $($('.menu-row')[$('.menu-row').length - 1]).css('margin-bottom', '25px');
+    /*
+    $('.menu-row').each(function() {
+        for (var i = 1; i < $(this).children('.menu-card').length - 1; i++) {
+            $($(this).children('.menu-card')[i]).css('margin-right', '25px').css('margin-left', '25px');
+        }
+    });
+    */
+    $('.menu-row').each(function() {
+        $(this).children('.menu-card').css('margin-right', '12px').css('margin-left', '12px');
+    });
+});
+
+for (var i = 1; i < $('.menu-row').length - 1; i++) {
+    $($('.menu-row')[i]).css('margin-top', '25px').css('margin-bottom', '25px');
+}
